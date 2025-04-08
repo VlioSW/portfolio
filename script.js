05ac0baacb5aff7f272808f0e00bb78d8ac9164a
vliotta <vin.liotta@free.fr>
2025-04-08 09:07:07 +0200

    v1.0.2

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+const themeButton = document.getElementById(`theme_button`);
+const themeImg = document.getElementById(`themeImg`);
+const canvas = document.getElementById('particles-js');
+
+let currentTheme = 'darkTheme';
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle('light');
+    if (currentTheme === 'darkTheme') {
+        themeImg.src = 'source/sun.svg';
+        currentTheme = 'lightTheme';
+    } else {
+        themeImg.src = 'source/moon.svg';
+        currentTheme = 'darkTheme';
+    }
+    }
+);
+
+
 const filterButtons = document.querySelectorAll('.filter');
-const projects = document.querySelectorAll('.project-card');
+const projects = document.querySelectorAll('.project_card');
 
 function filterProjects(attribute) {
     projects.forEach(project => {
@@ -17,3 +36,33 @@ filterButtons.forEach(button => {
         filterProjects(attribute);
     });
 });
+
+
+var swiper = new Swiper(".swiper", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    loop: true
+});
+
+const img = document.getElementById(project_img);
+
+function zoomImage(image) {
+    var popup = document.getElementById("popup");
+    var popup_img = document.getElementById("popup_img");
+    popup.style.display = "flex";
+    popup_img.src = image.src;
+}
+
+window.onclick = function(event) {
+    var popup = document.getElementById("popup");
+    var popup_content = document.querySelector(".popup-content");
+    if (event.target == popup) {
+        popup.style.display = "none";
+    }
+}
+
+img.addEventListener('click', function() {
+    zoomImage(img);
+})
